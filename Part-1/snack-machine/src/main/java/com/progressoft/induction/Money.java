43898a094e58fc163658d4ec3e6bde76ed49f80c
Shawki <salasy@sustainabilityexcellence.com>
2020-03-17 21:38:00 +0200

    Design and clean-up the code

---
+++ Part-1/snack-machine/src/main/java/com/progressoft/induction/Money.java
@@ -0,0 +1,74 @@
+package com.progressoft.induction;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Money {
+    public static Money ZERO = new Money(BigDecimal.valueOf(0));
+    public static Money QUARTER_DINAR = new Money(BigDecimal.valueOf(0.25));
+    public static Money HALF_DINAR = new Money(BigDecimal.valueOf(0.5));
+    public static Money DINAR = new Money(BigDecimal.valueOf(1.0));
+    public static Money FIVE_DINAR = new Money(BigDecimal.valueOf(1.0));
+    public static Money TEN_DINAR = new Money(BigDecimal.valueOf(1.0));
+    public static Money MAX_CAPACITY = new Money(BigDecimal.valueOf(1000.0));
+    private static HashMap<BigDecimal, Money> units = new HashMap<BigDecimal, Money>() {
+        {
+            put(BigDecimal.valueOf(0), Money.ZERO);
+            put(BigDecimal.valueOf(0.25), Money.QUARTER_DINAR);
+            put(BigDecimal.valueOf(1.0), Money.DINAR);
+            put(BigDecimal.valueOf(0.5), Money.HALF_DINAR);
+            put(BigDecimal.valueOf(5.0), Money.FIVE_DINAR);
+            put(BigDecimal.valueOf(10.0), Money.TEN_DINAR);
+        }
+    };
+    public BigDecimal pocket;
+
+    public Money(double money) {
+        this.pocket = BigDecimal.valueOf(money);
+    }
+
+    public Money(BigDecimal money) {
+        this.pocket = money;
+    }
+
+    public Money(Money money) {
+        this.pocket = money.get();
+    }
+
+    public Money add(Money money) {
+        this.pocket = this.pocket.add(money.get());
+        return new Money(this.pocket);
+    }
+
+    public Money subtract(Money money) {
+        this.pocket = this.pocket.subtract(money.get());
+        return new Money(this.pocket);
+    }
+
+    public BigDecimal get() {
+        return this.pocket;
+    }
+
+    public boolean isLessThan(Money money) {
+        return this.pocket.compareTo(money.get()) < 0;
+    }
+
+    public boolean isEqule(Money money) {
+        return this.pocket.compareTo(money.get()) == 0;
+    }
+
+    public boolean checkUnit(Money money) {
+        return (units.containsKey(money.get()));
+    }
+
+    public boolean checkUnit(double money) {
+        return (units.containsKey(BigDecimal.valueOf(money)));
+    }
+
+    public boolean checkCapacity(Money money) {
+        Money temp = new Money(this.pocket);
+        temp = temp.add(money);
+        return (temp.isLessThan(MAX_CAPACITY) || temp.isEqule(MAX_CAPACITY));
+    }
+}
